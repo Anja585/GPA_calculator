6b614322c4c975032cae8b9a4685815156d71276
Anja Antolkovic <anja.antolkovic@gmail.com>
2023-07-09 19:56:22 +0200

    some adjustments

--- gpa_calculator.py
+++ gpa_calculator.py
@@ -23,114 +23,141 @@ grade = {
         }
 
 # returns gpa from a user input
-def calculate_gpa(percentage_grades):
-    gpa_scale_grades = []
-    for percentage_grade in percentage_grades:
-        if percentage_grade >= 90:
-            gpa_scale_grades.append(grade["A+"])
-        elif percentage_grade >= 80:
-            gpa_scale_grades.append(grade["A"])
-        elif percentage_grade >= 70:
-            gpa_scale_grades.append(grade["A-"])
-        elif percentage_grade >= 67:
-            gpa_scale_grades.append(grade["B+"])
-        elif percentage_grade >= 64:
-            gpa_scale_grades.append(grade["B"])
-        elif percentage_grade >= 60:
-            gpa_scale_grades.append(grade["B-"])
-        elif percentage_grade >= 57:
-            gpa_scale_grades.append(grade["C+"])
-        elif percentage_grade >= 54:
-            gpa_scale_grades.append(grade["C"])
-        elif percentage_grade >= 50:
-            gpa_scale_grades.append(grade["C-"])
-        elif percentage_grade == 49:
-            gpa_scale_grades.append(grade["D+"])
-        elif percentage_grade >= 47:
-            gpa_scale_grades.append(grade["D"])
-        elif percentage_grade >= 45:
-            gpa_scale_grades.append(grade["D-"])
-        elif percentage_grade == 44:
-            gpa_scale_grades.append(grade["E+"])
-        elif percentage_grade >= 42:
-            gpa_scale_grades.append(grade["E"])
-        elif percentage_grade >= 40:
-            gpa_scale_grades.append(grade["E-"])
-        elif percentage_grade == 39:
-            gpa_scale_grades.append(grade["F+"])
-        else: gpa_scale_grades.append(grade["F"]) 
-    gpa = np.average(gpa_scale_grades)
-    gpa_rounded = np.round(gpa, 2)
-    return gpa_rounded
-        
+def calculate_gpa(final_list):
+    final_gpa = []
+    for i in final_list:
+        pairs = final_list[i]
+        gpa_scale_grades = []
+        for percentage_grade in pairs:
+            if percentage_grade >= 90:
+                gpa_scale_grades.append(grade["A+"])
+            elif percentage_grade >= 80:
+                gpa_scale_grades.append(grade["A"])
+            elif percentage_grade >= 70:
+                gpa_scale_grades.append(grade["A-"])
+            elif percentage_grade >= 67:
+                gpa_scale_grades.append(grade["B+"])
+            elif percentage_grade >= 64:
+                gpa_scale_grades.append(grade["B"])
+            elif percentage_grade >= 60:
+                gpa_scale_grades.append(grade["B-"])
+            elif percentage_grade >= 57:
+                gpa_scale_grades.append(grade["C+"])
+            elif percentage_grade >= 54:
+                gpa_scale_grades.append(grade["C"])
+            elif percentage_grade >= 50:
+                gpa_scale_grades.append(grade["C-"])
+            elif percentage_grade == 49:
+                gpa_scale_grades.append(grade["D+"])
+            elif percentage_grade >= 47:
+                gpa_scale_grades.append(grade["D"])
+            elif percentage_grade >= 45:
+                gpa_scale_grades.append(grade["D-"])
+            elif percentage_grade == 44:
+                gpa_scale_grades.append(grade["E+"])
+            elif percentage_grade >= 42:
+                gpa_scale_grades.append(grade["E"])
+            elif percentage_grade >= 40:
+                gpa_scale_grades.append(grade["E-"])
+            elif percentage_grade == 39:
+                gpa_scale_grades.append(grade["F+"])
+            else: gpa_scale_grades.append(grade["F"])
+        gpa = np.average(gpa_scale_grades)
+        gpa_rounded = np.round(gpa, 2)
+        final_gpa.append(gpa_rounded)
+    return final_gpa
+   
+
 # returns the name of the highest scoring module
-def highest_scoring_module(marks, module_names):
-    pairs = dict(zip(marks, module_names))
-    pair_keys = list(pairs.keys())
-    pair_keys.sort()
-    sorted_pairs = {i: pairs[i] for i in pair_keys}
-    highest_scoring_module = list(sorted_pairs.values())[-1]
-    return highest_scoring_module
+def highest_scoring_module(final_list):
+    highest_scoring_modules = []
+    for i in final_list:
+        pairs = final_list[i]
+        pair_keys = list(pairs.keys())
+        pair_keys.sort()
+        sorted_pairs = {i: pairs[i] for i in pair_keys}
+        highest_scoring_module = list(sorted_pairs.values())[-1]
+        highest_scoring_modules.append(highest_scoring_module)        
+    return highest_scoring_modules
 
 # returns the name of the lowest scoring module
-def lowest_scoring_module(marks, module_names):
-    pairs = dict(zip(marks, module_names))
-    pair_keys = list(pairs.keys())
-    pair_keys.sort()
-    sorted_pairs = {i: pairs[i] for i in pair_keys}
-    lowest_scoring_module = list(sorted_pairs.values())[0]
-    return lowest_scoring_module
+def lowest_scoring_module(final_list):
+    lowest_scoring_modules = []
+    for i in final_list:
+        pairs = final_list[i]
+        pair_keys = list(pairs.keys())
+        pair_keys.sort()
+        sorted_pairs = {i: pairs[i] for i in pair_keys}
+        lowest_scoring_module = list(sorted_pairs.values())[0]
+        lowest_scoring_modules.append(lowest_scoring_module)        
+    return lowest_scoring_modules
 
 # returns standard deviation
-def standard_deviation(marks):
-    st_deviation = np.std(marks)
-    st_deviation_rounded = round(st_deviation, 2) 
-    return st_deviation_rounded
+def standard_deviation(final_list):
+    standard_deviations = []
+    for i in final_list:
+        pairs = final_list[i]
+        pair_keys = list(pairs.keys())    
+        st_deviation = np.std(pair_keys)
+        st_deviation_rounded = round(st_deviation, 2) 
+        standard_deviations.append(st_deviation_rounded)
+    return standard_deviations
 
 # returns median
-def median(marks):
-    median = np.std(marks)
-    median_rounded = round(median, 2) 
-    return median_rounded
-   
+def median(final_list):
+    medians = []
+    for i in final_list:
+        pairs = final_list[i]
+        pair_keys = list(pairs.keys())  
+        median = np.median(pair_keys)
+        median_rounded = round(median, 2) 
+        medians.append(median_rounded)
+    return medians
+
 # return module names and letter grades
-def letter_grades(percentage_grades, module_names):
-    letter_grades = []
-    for percentage_grade in percentage_grades:
-        if percentage_grade >= 90:
-            letter_grades.append("A+")
-        elif percentage_grade >= 80:
-            letter_grades.append("A")
-        elif percentage_grade >= 70:
-            letter_grades.append("A-")
-        elif percentage_grade >= 67:
-            letter_grades.append("B+")
-        elif percentage_grade >= 64:
-            letter_grades.append("B")
-        elif percentage_grade >= 60:
-            letter_grades.append("B-")
-        elif percentage_grade >= 57:
-            letter_grades.append("C+")
-        elif percentage_grade >= 54:
-            letter_grades.append("C")
-        elif percentage_grade >= 50:
-            letter_grades.append("C-")
-        elif percentage_grade == 49:
-            letter_grades.append("D+")
-        elif percentage_grade >= 47:
-            letter_grades.append("D")
-        elif percentage_grade >= 45:
-            letter_grades.append("D-")
-        elif percentage_grade == 44:
-            letter_grades.append("E+")
-        elif percentage_grade >= 42:
-            letter_grades.append("E")
-        elif percentage_grade >= 40:
-            letter_grades.append("E-")
-        elif percentage_grade == 39:
-            letter_grades.append("F+")
-        else: letter_grades.append("F")
-    return dict(zip(letter_grades, module_names))
+def letter_grades(final_list):
+    final_results = []
+    for i in final_list:
+        pairs = final_list[i]
+        letter_grades = []
+        module_names = list(pairs.values())
+        for percentage_grade in pairs:
+            if percentage_grade >= 90:
+                letter_grades.append("A+")
+            elif percentage_grade >= 80:
+                letter_grades.append("A")
+            elif percentage_grade >= 70:
+                letter_grades.append("A-")
+            elif percentage_grade >= 67:
+                letter_grades.append("B+")
+            elif percentage_grade >= 64:
+                letter_grades.append("B")
+            elif percentage_grade >= 60:
+                letter_grades.append("B-")
+            elif percentage_grade >= 57:
+                letter_grades.append("C+")
+            elif percentage_grade >= 54:
+                letter_grades.append("C")
+            elif percentage_grade >= 50:
+                letter_grades.append("C-")
+            elif percentage_grade == 49:
+                letter_grades.append("D+")
+            elif percentage_grade >= 47:
+                letter_grades.append("D")
+            elif percentage_grade >= 45:
+                letter_grades.append("D-")
+            elif percentage_grade == 44:
+                letter_grades.append("E+")
+            elif percentage_grade >= 42:
+                letter_grades.append("E")
+            elif percentage_grade >= 40:
+                letter_grades.append("E-")
+            elif percentage_grade == 39:
+                letter_grades.append("F+")
+            else: letter_grades.append("F")
+        result = dict(zip(letter_grades, module_names))
+        final_results.append(result)
+    return final_results
 
 
 
@@ -139,23 +166,35 @@ def letter_grades(percentage_grades, module_names):
 # main function
 if __name__ == '__main__':
     os.system('cls')
-    marks = []
-    module_names = []
+    names = []
+    # marks = []
+    # module_names = []
+    marks_module_names = []
     while True:
-        module = str(input('Enter module name: '))
-        if module  == 'Y':
+        marks = []
+        module_names = []
+        name = str(input('Enter student name: '))
+        if name  == 'Y':
             break
-        grade_in_percentage = int(input('Enter grade (%): '))
-        module_names.append(module)
-        marks.append(grade_in_percentage)
-    # calculate_gpa(marks)
-    # highest_scoring_module(marks, module_names)
-    # lowest_scoring_module(marks, module_names)
-    # standard_deviation(marks):
-    # median(marks)
+        names.append(name)
+        while True:           
+            module = str(input('Enter module name: '))
+            if module  == 'Y':
+                break
+            grade_in_percentage = int(input('Enter grade (%): '))
+            if grade_in_percentage == 0:
+                break
+            module_names.append(module)
+            marks.append(grade_in_percentage)
+        marks_module_names.append(dict(zip(marks, module_names)))
+    final_list = dict(zip(names, marks_module_names))
+    # calculate_gpa(final_list)
+    # highest_scoring_module(final_list)
+    # lowest_scoring_module(final_list)
+    # standard_deviation(final_list)
+    # median(final_list)
+    # letter_grades(final_list)
     # for i in letter_grades(marks, module_names):
     #     module = str(letter_grades(marks, module_names)[i])
     #     print(f'{module}: {i}') 
-    
-
-
+    # print(final_list)
