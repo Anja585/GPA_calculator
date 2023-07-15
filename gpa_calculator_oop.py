2ce68e66c867b822be21751293d37f69f6406943
Anja Antolkovic <anja.antolkovic@gmail.com>
2023-07-15 12:56:09 +0200

    reconfiguration

--- gpa_calculator_oop.py
+++ gpa_calculator_oop.py
@@ -1,5 +1,87 @@
 import os
 import numpy as np
+import pandas as pd
+import tkinter.filedialog
+
+
+class Grade:
+    grade = { 
+        "A+": 4.2,
+        "A": 4,
+        "A-": 3.8,
+        "B+": 3.6, 
+        "B": 3.4, 
+        "B-": 3.2, 
+        "C+": 3, 
+        "C": 2.8,
+        "C-": 2.6, 
+        "D+": 2.4, 
+        "D": 2.2, 
+        "D-": 2, 
+        "E+": 1.8, 
+        "E": 1.6, 
+        "E-": 1.4, 
+        "F+": 1.2, 
+        "F": 1   
+        }
+    def __init__(self, percentage_grade):
+        self.percentage_grade = percentage_grade
+
+    def map_percentage_points_to_grades(self):
+        grade_keys = list(self.grade.keys())
+        if self.percentage_grade >= 90:
+            gpa_scale_grade = self.grade["A+"]
+            letter_grade = grade_keys[0]
+        elif self.percentage_grade >= 80:
+            gpa_scale_grade = self.grade["A"]
+            letter_grade = grade_keys[1]
+        elif self.percentage_grade >= 70:
+            gpa_scale_grade = self.grade["A-"]
+            letter_grade = grade_keys[2]
+        elif self.percentage_grade >= 67:
+            gpa_scale_grade = self.grade["B+"]
+            letter_grade = grade_keys[3]
+        elif self.percentage_grade >= 64:
+            gpa_scale_grade = self.grade["B"]
+            letter_grade = grade_keys[4]
+        elif self.percentage_grade >= 60:
+            gpa_scale_grade = self.grade["B-"]
+            letter_grade = grade_keys[5]
+        elif self.percentage_grade >= 57:
+            gpa_scale_grade = self.grade["C+"]
+            letter_grade = grade_keys[6]
+        elif self.percentage_grade >= 54:
+            gpa_scale_grade = self.grade["C"]
+            letter_grade = grade_keys[7]
+        elif self.percentage_grade >= 50:
+            gpa_scale_grade = self.grade["C-"]
+            letter_grade = grade_keys[8]
+        elif self.percentage_grade == 49:
+            gpa_scale_grade = self.grade["D+"]
+            letter_grade = grade_keys[9]
+        elif self.percentage_grade >= 47:
+            gpa_scale_grade = self.grade["D"]
+            letter_grade = grade_keys[10]
+        elif self.percentage_grade >= 45:
+            gpa_scale_grade = self.grade["D-"]
+            letter_grade = grade_keys[11]
+        elif self.percentage_grade == 44:
+            gpa_scale_grade = self.grade["E+"]
+            letter_grade = grade_keys[12]
+        elif self.percentage_grade >= 42:
+            gpa_scale_grade = self.grade["E"]
+            letter_grade = grade_keys[13]
+        elif self.percentage_grade >= 40:
+            gpa_scale_grade = self.grade["E-"]
+            letter_grade = grade_keys[14]
+        elif self.percentage_grade == 39:
+            gpa_scale_grade = self.grade["F+"]
+            letter_grade = grade_keys[15]
+        else: 
+            gpa_scale_grade = self.grade["F"]
+            letter_grade = "F" 
+        return gpa_scale_grade, letter_grade
+    
 
 class GPA_calculator:
     grade = { 
@@ -21,16 +103,15 @@ class GPA_calculator:
         "F+": 1.2, 
         "F": 1   
         }
-    def __init__(self, student_name, pairs):
-        self.student_name = student_name
-        self.pairs = pairs
-
-    def __repr__(self):
-        return f'({self.student_name}, {self.pairs})'
     
+    def __init__(self, student_name, **modules):
+        self.student_name = student_name
+        self.modules = modules
+
     def calculate_gpa(self):
             gpa_scale_grades = []
-            for percentage_grade in self.pairs:
+            self.module_values = list(self.modules.values())
+            for percentage_grade in self.module_values:
                 if percentage_grade >= 90:
                     gpa_scale_grades.append(self.grade["A+"])
                 elif percentage_grade >= 80:
@@ -65,39 +146,32 @@ class GPA_calculator:
                     gpa_scale_grades.append(self.grade["F+"])
                 else: gpa_scale_grades.append(self.grade["F"])
             gpa = np.average(gpa_scale_grades)
-            gpa_rounded = np.round(gpa, 2)
-            return gpa_rounded
-
-    def highest_scoring_module(self):
-        pair_keys = list(self.pairs.keys())
-        pair_keys.sort()
-        sorted_pairs = {i: self.pairs[i] for i in pair_keys}
-        highest_scoring_module = list(sorted_pairs.values())[-1]
-        return highest_scoring_module    
+            self.gpa_rounded = np.round(gpa, 2)
+            return self.student_name, self.gpa_rounded
     
+    def highest_scoring_module(self):
+        self.sorted_modules = dict(sorted(self.modules.items(), key=lambda x: x[1]))
+        highest_scoring_module = list(self.sorted_modules.keys())[-1]
+        return self.student_name, highest_scoring_module
+
     def lowest_scoring_module(self):
-        pair_keys = list(self.pairs.keys())
-        pair_keys.sort()
-        sorted_pairs = {i: self.pairs[i] for i in pair_keys}
-        lowest_scoring_module = list(sorted_pairs.values())[0]
-        return lowest_scoring_module
+        lowest_scoring_module = list(self.sorted_modules.keys())[0]
+        return self.student_name, lowest_scoring_module
     
     def standard_deviation(self):
-        pair_keys = list(self.pairs.keys())    
-        st_deviation = np.std(pair_keys)
+        st_deviation = np.std(self.module_values)
         st_deviation_rounded = round(st_deviation, 2) 
-        return st_deviation_rounded
+        return self.student_name, st_deviation_rounded
     
     def median(self):
-        pair_keys = list(self.pairs.keys())  
-        median = np.median(pair_keys)
+        median = np.median(self.module_values)
         median_rounded = round(median, 2) 
-        return median_rounded
+        return self.student_name, median_rounded
     
     def letter_grades(self):
             letter_grades = []
-            module_names = list(self.pairs.values())
-            for percentage_grade in self.pairs:
+            module_names = list(self.modules.keys())
+            for percentage_grade in self.module_values:
                 if percentage_grade >= 90:
                     letter_grades.append("A+")
                 elif percentage_grade >= 80:
@@ -131,49 +205,70 @@ class GPA_calculator:
                 elif percentage_grade == 39:
                     letter_grades.append("F+")
                 else: letter_grades.append("F")
-            result = dict(zip(letter_grades, module_names))
-            return result
-
-    def next_highest(self, gpas):
-        gpa_sorted = sorted(gpas)
-        for i in gpa_sorted:
-            if i > self.calculate_gpa():
-                difference = np.round((i - self.calculate_gpa()), 3)
+            result = dict(zip(module_names, letter_grades))
+            return self.student_name, result
+    
+    # double-check this function -and compare - still doesn't work
+    # def next_highest(self, students):
+    #     gpas = []
+    #     gpa_sorted = sorted(gpas)
+    #     for s in students:
+    #         name, gpa = s.calculate_gpa()
+    #         gpas.append(gpa)
+    #     for i in gpa_sorted:
+    #         if i > self.gpa_rounded:
+    #             difference = np.round((i - self.gpa_rounded), 3)
+    #     return self.student_name, difference
+                        
+def create_student():
+    final_list = []
+    file = str(input('Would you like to import a file (Y/N): '))
+    if file == 'Y':
+        final_list = file_import()
+        return final_list
+    else:
+        while True:
+            marks = []
+            module_names = []            
+            print('To quit or insert data for another student press 0')
+            name = str(input('Enter student name: '))
+            if name  == '0':
                 break
-            elif 4.2 > self.calculate_gpa():
-                difference = np.round((4.2 - self.calculate_gpa()), 3)
-        return difference          
+            while True:           
+                module = str(input('Enter module name: '))
+                if module  == '0':
+                    break
+                try:
+                    grade_in_percentage = int(input('Enter grade (%): '))
+                    if grade_in_percentage  == 0:
+                        break
+                    marks.append(grade_in_percentage)    
+                except:
+                    ValueError
+                    print('Use number characters only!')
+                module_names.append(module)            
+            marks_module_names = dict(zip(module_names, marks))
+            s = GPA_calculator(name, **marks_module_names)
+            final_list.append(s) 
+    return final_list
+
+def file_import():
+    final_list = []
+    filename = tkinter.filedialog.askopenfile()
+    df = pd.read_csv(filename, index_col=0)
+    dict_file = df.to_dict('index')
+    for i in dict_file:
+        s = GPA_calculator(i, **dict_file[i])
+        final_list.append(s)
+    return final_list
+
+
             
                 
 # main function
 if __name__ == '__main__':
     os.system('cls')    
-    names = []
-    marks_module_names = []
-    while True:
-        marks = []
-        module_names = []
-        print('To quit or insert data for another student press 0')
-        name = str(input('Enter student name: '))
-        if name  == '0':
-            break
-        names.append(name)
-        while True:           
-            module = str(input('Enter module name: '))
-            if module  == '0':
-                break
-            try:
-                grade_in_percentage = int(input('Enter grade (%): '))
-                if grade_in_percentage  == 0:
-                    break
-                marks.append(grade_in_percentage)    
-            except:
-                ValueError
-                print('Use number characters only!')
-            module_names.append(module)            
-        marks_module_names.append(dict(zip(marks, module_names)))
-    final_list = dict(zip(names, marks_module_names))    
-    gpa_objects = []
+    student_objects = create_student() 
     gpas = []
     highest_scoring_modules = []
     lowest_scoring_modules = []
@@ -181,43 +276,39 @@ if __name__ == '__main__':
     medians = []
     next_highest = []
     letter_grades = []
-    for i in final_list:
-        gpa_object = GPA_calculator(i, final_list[i])
-        gpa_objects.append(gpa_object)
-    for i in gpa_objects:
+    for i in student_objects:
         gpa = i.calculate_gpa()
         gpas.append(gpa)
         highest_scoring_module = i.highest_scoring_module()
         highest_scoring_modules.append(highest_scoring_module)
-        lowest_scoring_module = i.highest_scoring_module()
+        lowest_scoring_module = i.lowest_scoring_module()
         lowest_scoring_modules.append(lowest_scoring_module)
-        st_deviation = i.standard_deviation()
-        st_deviations.append(st_deviation)
+        standard_deviation = i.standard_deviation()
+        st_deviations.append(standard_deviation)
         median = i.median()
         medians.append(median)
+        # print(i.next_highest(student_objects))
+        # next_highest.append(i)
         letter_grade = i.letter_grades()
         letter_grades.append(letter_grade)
-    for i in gpa_objects:
-        next_high = i.next_highest(gpas)
-        next_highest.append(next_high)
-    if len(list(final_list.keys())) == 0:
+    if len(student_objects) == 0:
         print('Not enough data')
         print('End')
     else: 
         print('\nGPA')
-        print(f'{dict(zip(names, gpas))}\n') 
+        print(f'{dict(gpas)}\n') 
         print('Highest scoring module')
-        print(f'{dict(zip(names, highest_scoring_modules))}\n') 
+        print(f'{dict(highest_scoring_modules)}\n') 
         print('Lowest scoring module')
-        print(f'{dict(zip(names, lowest_scoring_modules))}\n')
+        print(f'{dict(lowest_scoring_modules)}\n')
         print('Standard deviation')
-        print(f'{dict(zip(names, st_deviations))}\n')
+        print(f'{dict(st_deviations)}\n')
         print('Median')
-        print(f'{dict(zip(names, medians))}\n')
-        print('Gap from the next highest GPA')       
-        print(f'{dict(zip(names, next_highest))}\n')
+        print(f'{dict(medians)}\n')
+        print('Grade point gap to the next highest GPA')       
+        # print(f'{dict(next_highest)}\n')
         print('Letter grades')
-        print(f'{dict(zip(names, letter_grades))}\n')
+        print(f'{dict(letter_grades)}\n')
         
         
     
